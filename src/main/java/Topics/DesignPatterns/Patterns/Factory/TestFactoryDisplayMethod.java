a6136de9704915754aa5a0f93f53716eeece92e0
Jacob Graham <jakegraham54@gmail.com>
2022-04-22 20:56:57 -0400

    Added justification for using the factory design pattern when designing software

--- src/main/java/Topics/DesignPatterns/Patterns/Factory/TestFactoryDisplayMethod.java
+++ src/main/java/Topics/DesignPatterns/Patterns/Factory/TestFactoryDisplayMethod.java
@@ -1,5 +1,16 @@
 package Topics.DesignPatterns.Patterns.Factory;
 
+/**
+ * The below implementation works by letting the subclasses implement the various factory methods. By doing this, you can create different instances of XML parsers depending on
+ * the one that is needed dynamically at runtime when required.
+ */
+
+/**
+ * When to use the factory method:
+ * A class cannot anticipate the class of objects it must create
+ * A class wants its subclasses to specify the objects it creates
+ * Classes delegate responsibility to one of several helper subclasses, and you want to localize the knowledge of which helper subclass is the delegate.
+ */
 public class TestFactoryDisplayMethod {
 
     public static void main(String[] args) {
